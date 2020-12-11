10306d06f7af2928a6b2e76b6b26d4cb36f4cdd9
Mikicaa97 <60716689+Mikicaa97@users.noreply.github.com>
2020-12-11 13:18:32 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,24 @@ document.getElementById("button").addEventListener("click", function(){
 document.querySelector(".close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 })
+
+
+//forma
+const form =document.getElementById('form');
+const username =document.getElementById('username');
+const password =document.getElementById('password');
+const email =document.getElementById('email');
+const password2 =document.getElementById('password2');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs(){
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const password2Value = password2.value.trim()
+}
