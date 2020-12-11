6a137bf216093f70358702da2f1039c4f4be60a9
Mikicaa97 <60716689+Mikicaa97@users.noreply.github.com>
2020-12-11 15:35:49 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,48 +2,54 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-//Display mobile menu
 const mobileMenu = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-}
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
 
-menu.addEventListener('click' , mobileMenu);
+menu.addEventListener('click', mobileMenu);
 
-//Aktivni meni kad skroluje
 const highlightMenu = () => {
-    const elem = document.querySelector('highlight');
-    const homeMenu = document.querySelector('home-page');
-    const aboutMenu = document.querySelector('about-page');
-    const servicesMenu = document.querySelector('services-page');
-    let scrollPos = window.scrollY
-    console.log(scrollPos);
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
+  console.log(scrollPos);
 
-    if(window.innerHeight > 960 && scrollPos < 600){ 
-        homeMenu.classList.add('navbar__links');
-        aboutMenu.classList.remove('navbar__links');
-        return
-    }
-    else if(window.innerHeight > 960 && scrollPos < 1400){
-        aboutMenu.classList.add('navbar__links');
-        homeMenu.classList.remove('navbar__links');
-        servicesMenu.classList.remove('navbar__links');
-        return
-    }
-    else if(window.innerHeight > 960 && scrollPos < 2345){
-        servicesMenu.classList.add('navbar__links');
-        aboutMenu.classList.remove('navbar__links');
-        return
-    }
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
 
-    if(elem && window.innerHeight < 960 && scrollPos < 600 || elem){
-        elem.classList.remove('navbar__links');
-    }
-}
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
 
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
 
 //popup
 
@@ -127,21 +133,3 @@ function prikaziTacnoZa(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
-
-
-
-$(document).ready(function () {
-  
-    $('.first-button').on('click', function () {
-    
-      $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
-    
-      $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
-    
-      $('.animated-icon3').toggleClass('open');
-    });
-    });
